64a01009bafa5b36c2fda8e19bac726189a7c903
julio_filizzola <juliofilizzola@hotmail.com>
2023-03-10 19:29:35 -0300

    Feat: creat sql.script

---
+++ api/sql/sql.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS devbook;
+USE devbook;
+DROP TABLE IF EXISTS user;
+CREATE TABLE USER (
+    id int auto_increment primary key,
+    name varchar(50) not null,
+    nick varchar(50) not null unique,
+    email varchar(50) not null unique ,
+    password varchar(20)not null unique ,
+    created_at timestamp default current_timestamp(),
+    updated_at timestamp default null
+) ENGINE=INNODB;
