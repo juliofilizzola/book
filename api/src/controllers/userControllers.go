2523e3de3c617caaf19bc63fb4919026ff50b779
julio_filizzola <juliofilizzola@hotmail.com>
2023-03-11 18:58:24 -0300

    Feat: config envs

--- api/src/controllers/userControllers.go
+++ api/src/controllers/userControllers.go
@@ -1,9 +1,37 @@
 package UserControllers
 
-import "net/http"
+import (
+	"api/db"
+	"api/src/models"
+	"api/src/repositories"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Creater User"))
+	body, error := io.ReadAll(r.Body)
+	if error != nil {
+		log.Fatal(error)
+	}
+	var user models.User
+	if error = json.Unmarshal(body, &user); error != nil {
+		log.Fatal(error)
+	}
+
+	db2, err := db.Connection()
+	if err != nil {
+		log.Fatal(error)
+	}
+	repo := repositories.UserRepository(db2)
+	create, err := repo.Create(user)
+	if err != nil {
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("id insert: %d", create)))
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
