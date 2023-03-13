d41bd49bbf0a2ca01ca6c5759f3320ef31229ebd
julio_filizzola <juliofilizzola@hotmail.com>
2023-03-13 12:48:00 -0300

    Feat: create func get user by id

--- api/src/repositories/user.go
+++ api/src/repositories/user.go
@@ -39,7 +39,7 @@ func (u User) Create(user models.User) (uint64, error) {
 func (u User) GetUsers(params string) ([]models.User, error) {
 	param := fmt.Sprintf("%%%s%%", params)
 	query, err := u.db.Query(
-		"select id, name, nick, email, created_at, updated_at from user where name LIKE ? or nick LIKE ?", param, param,
+		"select id, name, nick, email, created_at from user where name LIKE ? or nick LIKE ?", param, param,
 	)
 	if err != nil {
 		return nil, err
@@ -55,13 +55,11 @@ func (u User) GetUsers(params string) ([]models.User, error) {
 
 	for query.Next() {
 		var user models.User
-		fmt.Println(user)
 		if err = query.Scan(
 			&user.ID,
 			&user.Name,
 			&user.Nick,
 			&user.Email,
-			&user.UpdatedAt,
 			&user.CreatedAt,
 		); err != nil {
 			return nil, err
@@ -70,4 +68,33 @@ func (u User) GetUsers(params string) ([]models.User, error) {
 		users = append(users, user)
 	}
 	return users, nil
+}
+
+func (u User) GetUser(ID uint64) (models.User, error) {
+
+	getUser, err := u.db.Query("select id, name, nick, email, created_at from user where id = ? ", ID)
+
+	if err != nil {
+		return models.User{}, err
+	}
+
+	defer getUser.Close()
+
+	var user models.User
+
+	if getUser.Next() {
+		if err = getUser.Scan(
+			&user.ID,
+			&user.Name,
+			&user.Nick,
+			&user.Email,
+			&user.CreatedAt,
+		); err != nil {
+			return models.User{}, err
+		}
+	} else {
+		return models.User{}, err
+	}
+
+	return user, nil
 }
