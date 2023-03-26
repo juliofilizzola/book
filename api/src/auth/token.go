315e79dc71aaf402578c13c3ad9240b25da2b942
julio_filizzola <juliofilizzola@hotmail.com>
2023-03-25 21:11:52 -0300

    feat: create func Generate Token

---
+++ api/src/auth/token.go
@@ -0,0 +1,16 @@
+package auth
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+func GenerateToken(userId uint64) (string, error) {
+	permission := jwt.MapClaims{}
+	permission["authorized"] = true
+	permission["exp"] = time.Now().Add(time.Hour * 6).Unix()
+	permission["userId"] = userId
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permission)
+	return token.SignedString([]byte("secret"))
+}
