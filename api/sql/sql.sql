108aaac7b9c2cbb20bf3046fd075057a00deffde
julio_filizzola <juliofilizzola@hotmail.com>
2023-04-16 09:48:37 -0300

    feat: CREATE TABLE FOLLOWERS

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -1,6 +1,11 @@
 CREATE DATABASE IF NOT EXISTS `dev-book`;
+
 USE `dev-book`;
+
 DROP TABLE IF EXISTS user;
+
+DROP TABLE IF EXISTS FOLLOWERS;
+
 CREATE TABLE USER (
     id int auto_increment primary key,
     name varchar(50) not null,
@@ -9,4 +14,18 @@ CREATE TABLE USER (
     password varchar(100) not null,
     created_at timestamp default current_timestamp(),
     updated_at timestamp default null
+) ENGINE=INNODB;
+
+CREATE TABLE FOLLOWERS(
+    user_id int not null,
+    FOREIGN KEY (user_id)
+    REFERENCES USER(id)
+    ON DELETE CASCADE,
+
+    follower_id int not null,
+    FOREIGN KEY (follower_id)
+    REFERENCES USER(id)
+    ON DELETE CASCADE,
+
+    PRIMARY KEY (user_id, follower_id)
 ) ENGINE=INNODB;
