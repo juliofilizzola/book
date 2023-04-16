f4144bc9405bb6259a8185991756af156d01ae4c
julio_filizzola <juliofilizzola@hotmail.com>
2023-04-16 11:27:12 -0300

    feat: create controller

--- api/src/controllers/followers/followersController.go
+++ api/src/controllers/followers/followersController.go
@@ -1,7 +1,59 @@
 package FollowersController
 
-import "net/http"
+import (
+	db2 "api/db"
+	"api/src/auth"
+	followersRepository "api/src/repositories/followers"
+	"api/src/response"
+	"database/sql"
+	"errors"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+)
 
 func FollowerUser(w http.ResponseWriter, r *http.Request) {
+	followerId, err := auth.GetIdToken(r)
+
+	if err != nil {
+		response.Err(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	params := mux.Vars(r)
+
+	userId, err := strconv.ParseUint(params["userId"], 10, 64)
+
+	if err != nil {
+		response.Err(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if userId == followerId {
+		response.Err(w, http.StatusForbidden, errors.New("cannot follower you"))
+		return
+	}
+
+	db, err := db2.Connection()
+
+	if err != nil {
+		response.Err(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			response.Err(w, http.StatusInternalServerError, err)
+			return
+		}
+	}(db)
+
+	repo := followersRepository.FollowersRepository(db)
+
+	if err = repo.Followers(userId, followerId); err != nil {
+		response.Err(w, http.StatusBadRequest, err)
+		return
+	}
 
 }
