cc41cd4550ed7db57fb953eb47760160734b3aa3
julio_filizzola <juliofilizzola@hotmail.com>
2023-04-15 21:47:12 -0300

    feat: create valid_user.go

---
+++ api/src/validation/valid_user/valid_user.go
@@ -0,0 +1,21 @@
+package validation
+
+import (
+	"api/src/auth"
+	"api/src/response"
+	"net/http"
+)
+
+func ValidUser(r *http.Request, w http.ResponseWriter, ID uint64) error {
+	userIdToken, err := auth.GetIdToken(r)
+
+	if err != nil {
+		response.Err(w, http.StatusUnauthorized, err)
+	}
+
+	if userIdToken != ID {
+		response.Err(w, http.StatusUnauthorized, err)
+	}
+
+	return nil
+}
