15e0f0a377d64c2a1ad2ff9381b7f562f22b8d5e
julio_filizzola <juliofilizzola@hotmail.com>
2023-04-22 10:43:10 -0300

    ref: comment valid user

--- api/src/validation/valid_user/valid_user.go
+++ api/src/validation/valid_user/valid_user.go
@@ -1,20 +1,22 @@
 package validation
 
 import (
-	"api/src/auth"
-	"api/src/response"
 	"net/http"
 )
 
-func ValidUser(r *http.Request, w http.ResponseWriter, ID uint64) {
-	userIdToken, err := auth.GetIdToken(r)
-
-	if err != nil {
-		response.Err(w, http.StatusUnauthorized, err)
-	}
-
-	if userIdToken != ID {
-		response.Err(w, http.StatusForbidden, err)
-	}
-
+func ValidUser(r *http.Request, w http.ResponseWriter, ID uint64) bool {
+	//userIdToken, err := auth.GetIdToken(r)
+	//fmt.Printf(strconv.FormatUint(userIdToken, 10), "token")
+	//fmt.Println(err, "this is err")
+	//if err != nil {
+	//	response.Err(w, http.StatusUnauthorized, err)
+	//	return false
+	//}
+	//
+	//if userIdToken != ID {
+	//	response.Err(w, http.StatusForbidden, err)
+	//	return false
+	//
+	//}
+	return true
 }
