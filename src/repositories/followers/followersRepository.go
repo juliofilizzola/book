d6b8474b82c163a34e0de007f1271dc74cfc6877
julio_filizzola <juliofilizzola@hotmail.com>
2023-05-11 09:09:09 -0300

    feat: create Repo get followers

--- src/repositories/followers/followersRepository.go
+++ src/repositories/followers/followersRepository.go
@@ -85,3 +85,35 @@ func (u User) GetFollow(userId uint64) ([]models.User, error) {
 	}
 	return users, nil
 }
+
+func (u User) GetFollowers(followId uint64) ([]models.User, error) {
+	row, err := u.db.Query(`
+		select user.id, user.name, user.email, user.nick, user.created_at
+		from USER inner join FOLLOWERS F on user.id = F.follower_id where F.follower_id = ?
+	`, followId)
+	if err != nil {
+		return nil, err
+	}
+	defer func(row *sql.Rows) {
+		err := row.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(row)
+
+	var users []models.User
+	for row.Next() {
+		var user models.User
+		if err = row.Scan(
+			&user.ID,
+			&user.Email,
+			&user.Name,
+			&user.Nick,
+			&user.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+	return users, nil
+}
