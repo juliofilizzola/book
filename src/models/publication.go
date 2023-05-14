f8ac17efa7bcd6a87464c03d422fd6b32d043810
julio_filizzola <juliofilizzola@hotmail.com>
2023-05-14 15:36:11 -0300

    feat: add Description from model Publication

--- src/models/publication.go
+++ src/models/publication.go
@@ -1,11 +1,12 @@
 package models
 
 type Publication struct {
-	ID        uint64 `json:"id,omitempty"`
-	Title     string `json:"title,omitempty"`
-	Content   string `json:"content,,omitempty"`
-	AuthId    string `json:"auth_id,omitempty"`
-	AuthNick  string `json:"auth_nick,omitempty"`
-	Likes     string `json:"likes"`
-	CreatedAt string `json:"created_at,omitempty"`
+	ID          uint64 `json:"id,omitempty"`
+	Title       string `json:"title,omitempty"`
+	Content     string `json:"content,omitempty"`
+	Description string `json:"description"`
+	AuthId      string `json:"auth_id,omitempty"`
+	AuthNick    string `json:"auth_nick,omitempty"`
+	Likes       string `json:"likes"`
+	CreatedAt   string `json:"created_at,omitempty"`
 }
