c4a6b73ab38544f8a4d7da746c4022d05f9df86b
julio_filizzola <juliofilizzola@hotmail.com>
2023-05-16 07:44:15 -0300

    feat: create funcs from get and get all publication

--- src/repositories/publication/publication.repository.go
+++ src/repositories/publication/publication.repository.go
@@ -42,3 +42,81 @@ func (p Publication) Create(Publication models.Publication) (uint64, error) {
 
 	return uint64(idInsert), nil
 }
+
+func (p Publication) GetPublicationByUser(id uint64) ([]models.PublicationReturn, error) {
+	query, err := p.db.Query(`select
+    	P.id, P.title, P.auth_id, P.description, P.content, P.'like', P.created_at, U.id as AuthId, U.name as AuthName, U.nick as AuthNick, U.email as AuthEmail
+		from PUBLICATION as P inner join USER U on P.auth_id = U.id where auth_id = ?`, id)
+
+	defer func(query *sql.Rows) {
+		err := query.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var publications []models.PublicationReturn
+	for query.Next() {
+		var publication models.PublicationReturn
+		if err = query.Scan(
+			&publication.ID,
+			&publication.Title,
+			&publication.Content,
+			&publication.Description,
+			&publication.AuthId,
+			&publication.AuthName,
+			&publication.AuthEmail,
+			&publication.AuthNick,
+			&publication.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+		publications = append(publications, publication)
+	}
+	return publications, nil
+}
+
+func (p Publication) GetPublication() ([]models.PublicationReturn, error) {
+	query, err := p.db.Query(`select
+    	P.id, P.title, P.auth_id, P.description, P.content, P.'like', P.created_at, U.id as AuthId, U.name as AuthName, U.nick as AuthNick, U.email as AuthEmail
+		from PUBLICATION as P inner join USER U on P.auth_id = U.id`)
+
+	defer func(query *sql.Rows) {
+		err := query.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var publications []models.PublicationReturn
+	for query.Next() {
+		var publication models.PublicationReturn
+		if err = query.Scan(
+			&publication.ID,
+			&publication.Title,
+			&publication.Content,
+			&publication.Description,
+			&publication.AuthId,
+			&publication.AuthName,
+			&publication.AuthEmail,
+			&publication.AuthNick,
+			&publication.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+		publications = append(publications, publication)
+	}
+	return publications, nil
+}
+
+//func (p Publication) UpdatePublication() error {
+//
+//}
