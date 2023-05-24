0a9e593074d7394e993dc69af01c7bf885d86a3b
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-05-24 10:12:59 -0300

    feat: remove print

--- src/auth/validUser.auth.go
+++ src/auth/validUser.auth.go
@@ -1,14 +1,12 @@
 package auth
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func ValidUser(r *http.Request, userId uint64) bool {
 	userIdToken, err := GetUserId(r)
 	if err != nil {
-		fmt.Printf("oi")
 		return false
 	}
 
