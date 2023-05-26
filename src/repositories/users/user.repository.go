f70cf4bc29ba40216ebcc7ca234422ca08ca45b1
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-05-26 16:08:04 -0300

    ref: modify get users repository

--- src/repositories/users/user.repository.go
+++ src/repositories/users/user.repository.go
@@ -63,37 +63,51 @@ func (u User) Create(user models.User) (string, error) {
 }
 
 func (u User) GetUsers(params string) ([]models.User, error) {
+	ctx := context.Background()
+
 	param := fmt.Sprintf("%%%s%%", params)
-	query, err := u.db.Query(
-		"select id, name, nick, email, created_at from user where name LIKE ? or nick LIKE ?", param, param,
+
+	var (
+		users []models.User
 	)
+
+	err := u.db.Prisma.QueryRaw("select id, name, nick, email, created_at from user where name LIKE ? or nick LIKE ?", param, param).Exec(ctx, &users)
+
 	if err != nil {
 		return nil, err
 	}
 
-	defer func(query *sql.Rows) {
-		err := query.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(query)
-	var users []models.User
-
-	for query.Next() {
-		var user models.User
-		if err = query.Scan(
-			&user.ID,
-			&user.Name,
-			&user.Nick,
-			&user.Email,
-			&user.CreatedAt,
-		); err != nil {
-			return nil, err
-		}
-
-		users = append(users, user)
-	}
 	return users, nil
+	//query, err := u.db.Query(
+	//	"select id, name, nick, email, created_at from user where name LIKE ? or nick LIKE ?", param, param,
+	//)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//defer func(query *sql.Rows) {
+	//	err := query.Close()
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//}(query)
+	//var users []models.User
+	//
+	//for query.Next() {
+	//	var user models.User
+	//	if err = query.Scan(
+	//		&user.ID,
+	//		&user.Name,
+	//		&user.Nick,
+	//		&user.Email,
+	//		&user.CreatedAt,
+	//	); err != nil {
+	//		return nil, err
+	//	}
+	//
+	//	users = append(users, user)
+	//}
+	//return users, nil
 }
 
 func (u User) GetUser(ID uint64) (models.User, error) {
