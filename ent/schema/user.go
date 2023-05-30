2f984c1a72694919bd5fafb4bb6f88a9d66391fa
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-05-30 09:39:27 -0300

    feat: create schema user

---
+++ ent/schema/user.go
@@ -0,0 +1,20 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("age").
+			Positive(),
+		field.String("name").
+			Default("unknown"),
+	}
+}
