2758458718f1f6dc9bf1819001f8562669c31d69
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-06-03 18:19:47 -0300

    feat: modify file auth.repository.go

--- cmd/repositories/auth/auth.repository.go
+++ cmd/repositories/auth/auth.repository.go
@@ -1,15 +1,16 @@
 package authRepository
 
 import (
-	"api/prisma/db"
-	"context"
+	"database/sql"
+	"errors"
+	"log"
 )
 
 type Auth struct {
-	db *db.PrismaClient
+	db *sql.DB
 }
 
-func AuthRepository(db *db.PrismaClient) *Auth {
+func AuthRepository(db *sql.DB) *Auth {
 	return &Auth{
 		db: db,
 	}
@@ -19,64 +20,64 @@ func (a Auth) SearchPassword(userId string) (string, error) {
 	var (
 		password = ""
 	)
-	ctx := context.Background()
+	//ctx := context.Background()
 
-	//query, err := a.db.Query(
-	//	"select password from user where user.id = ? ", param,
-	//)
+	query, err := a.db.Query(
+		"select password from user where user.id = ? ", userId,
+	)
 
-	result, err := a.db.User.FindFirst(
-		db.User.ID.Equals(userId),
-	).Exec(ctx)
-
-	if err != nil {
-		return password, err
-	}
-	password = result.Password
-	//defer func(query *sql.Rows) {
-	//	err := query.Close()
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-	//}(query)
-
-	//if query.Next() {
-	//	if err = query.Scan(
-	//		&password,
-	//	); err != nil {
-	//		return password, err
-	//	}
-	//} else {
-	//	return password, errors.New("not found")
+	//result, err := a.db.User.FindFirst(
+	//	db.User.ID.Equals(userId),
+	//).Exec(ctx)
+	//
+	//if err != nil {
+	//	return password, err
 	//}
+	//password = result.Password
+	defer func(query *sql.Rows) {
+		err := query.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(query)
+
+	if query.Next() {
+		if err = query.Scan(
+			&password,
+		); err != nil {
+			return password, err
+		}
+	} else {
+		return password, errors.New("not found")
+	}
 	return password, nil
 }
 
 func (a Auth) UpdatePassword(newPassword []byte, userId string) error {
-	ctx := context.Background()
+	//ctx := context.Background()
+	//
+	//_, err := a.db.User.FindUnique(
+	//	db.User.ID.Equals(userId),
+	//).Update(
+	//	db.User.Password.Set(string(newPassword)),
+	//).Exec(ctx)
 
-	_, err := a.db.User.FindUnique(
-		db.User.ID.Equals(userId),
-	).Update(
-		db.User.Password.Set(string(newPassword)),
-	).Exec(ctx)
-
-	//statement, err := a.db.Prepare("update user set password = ? where user.id = ?")
+	statement, err := a.db.Prepare("update user set password = ? where user.id = ?")
 
 	if err != nil {
 		return err
 	}
 	//
-	//defer func(statement *sql.Stmt) {
-	//	err := statement.Close()
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-	//}(statement)
-	//
-	//if _, err := statement.Exec(newPassword, userId); err != nil {
-	//	return err
-	//}
+	defer func(statement *sql.Stmt) {
+		err := statement.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(statement)
+
+	if _, err := statement.Exec(newPassword, userId); err != nil {
+		return err
+	}
 
 	return nil
 }
